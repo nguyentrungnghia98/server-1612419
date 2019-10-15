462c31de21305a48aa6d99ec6e8e82899abc0f0c
unknown <ntn641998@gmail.com>
2019-10-15 15:38:37 +0700

    test deploy

--- routes/index.js
+++ routes/index.js
@@ -1,13 +1,14 @@
 var express = require('express');
 var router = express.Router();
 var User = require("../models/User")
+
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('login', { layout: 'login.handlebars',script: "login", style: "login" });
 });
 
 router.get('/login', async function(req, res, next) {
-  const users = await User.find({});
+  const users = await (3+2);
   res.render('login', { layout: 'login.handlebars', users, script: "login", style: "login" });
 });
 
